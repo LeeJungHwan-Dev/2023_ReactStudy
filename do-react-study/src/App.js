2022c3225529732943e3074cddde926fad522f0e
JungHwan_Lee <93726941+LeeJungHwan-Dev@users.noreply.github.com>
2023-01-09 23:22:51 +0900

    Update : App.js

--- do-react-study/src/App.js
+++ do-react-study/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { Component } from 'react';
+import TodaysPlan from './03/TodaysPlan';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1 className="title">두잇! 리액트 시작하기</h1>
+      <div className="body">
+        <TodaysPlan />
       </div>
     );
   }
